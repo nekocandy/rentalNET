2e4b7f081f9a7d0369fe02adbd931af03cae65f2
Jeevika <heevk23499@gmail.com>
2024-01-30 16:04:35 +0530

    feat: show logo on login page

--- components/BottomBar.vue
+++ components/BottomBar.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
 import FlowImage from '~/assets/flow.svg'
+
+const router = useRouter()
+const shouldShowLogo = computed(() => {
+  return !isUserLoggedIn.value || router.currentRoute.value.path === '/'
+})
 </script>
 
 <template>
   <div border-2 border-teal-900 px-8 py-4>
-    <div v-if="isUserLoggedIn">
-      {{ isUserLoggedIn }}
-    </div>
-
-    <div v-else flex items-center gap-4>
+    <div v-if="shouldShowLogo" flex items-center gap-4>
       <span text-white text-3xl font-bold font-mono>
         Made with
       </span>
       <img h-12 :src="FlowImage" alt="">
+    </div>
+
+    <div v-else>
+      {{ userData }}
     </div>
   </div>
 </template>
