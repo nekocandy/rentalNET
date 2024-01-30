a31283e02dad3cc2551cb5f67836feabeacd64a8
Shalini <shalini5640j@gmail.com>
2024-01-30 15:54:16 +0530

    feat: add login

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div flex flex-col gap-8 h-screen py-8 px-12 class="bg-teal-900/80">
+  <div flex flex-col gap-8 h-screen py-8 px-12 class="bg-teal-900/80" font-mono text-white>
     <main flex-1 bg-teal-900>
       <slot />
     </main>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
+import FlowImage from '~/assets/flow.svg'
+import { logIn } from '~/utils/flow/auth'
 
+async function login() {
+  await logIn()
+}
 </script>
 
 <template>
-  <div flex-col gap-12 class=" font-mono text-black">
-    <h1 />
+  <div flex flex-col h-full items-center justify-center gap-12>
+    <div v-if="!isUserLoggedIn">
+      <button flex items-end gap-4 px-4 bg-teal-800 px-6 py-4 border-2 @click="login">
+        <div text-2xl font-bold>
+          Log in with
+        </div>
+        <img h-8 :src="FlowImage" alt="">
+      </button>
+    </div>
   </div>
 </template>
 
