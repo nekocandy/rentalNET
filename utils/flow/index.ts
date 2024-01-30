2999af05a22d14e09bfd084b3fdca3a88aa7b4a4
Chaaya <chayalan72@gmail.com>
2024-01-30 11:03:19 +0530

    feat: add index

---
+++ utils/flow/index.ts
@@ -0,0 +1,35 @@
+import { config } from '@onflow/fcl'
+import flowJSON from '~/flow.json'
+
+const runtimeConfig = useRuntimeConfig().public
+export const network: 'mainnet' | 'testnet' | 'emulator' = (runtimeConfig.NETWORK as 'mainnet' | 'testnet' | 'emulator') || 'testnet'
+
+const fclConfigInfo = {
+  emulator: {
+    accessNode: 'http://127.0.0.1:8888',
+    discoveryWallet: 'http://localhost:8701/fcl/authn',
+    discoveryAuthInclude: [],
+  },
+  testnet: {
+    accessNode: 'https://rest-testnet.onflow.org',
+    discoveryWallet: 'https://fcl-discovery.onflow.org/testnet/authn',
+    discoveryAuthInclude: [],
+  },
+  mainnet: {
+    accessNode: 'https://rest-mainnet.onflow.org',
+    discoveryWallet: 'https://fcl-discovery.onflow.org/authn',
+    discoveryAuthInclude: [],
+  },
+}
+
+const { NC_ADDRESS } = runtimeConfig
+
+config({
+  'app.detail.title': 'Kirai',
+  'app.detail.icon': 'https://pycz.dev/favicon.svg',
+  'flow.network': network,
+  'accessNode.api': fclConfigInfo[network].accessNode,
+  'discovery.wallet': fclConfigInfo[network].discoveryWallet,
+  'discovery.authn.include': fclConfigInfo[network].discoveryAuthInclude,
+  '0xNC': NC_ADDRESS,
+}).load({ flowJSON })
