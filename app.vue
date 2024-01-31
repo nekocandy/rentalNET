3afedf78e66f4b6e77628af2cce00f8838c1df56
Shalini <shalini5640j@gmail.com>
2024-01-31 11:04:24 +0530

    feat: check iframe and add transaction dialog

--- app.vue
+++ app.vue
@@ -1,10 +1,27 @@
 <script lang="ts" setup>
 import { Notifications, Notivue } from 'notivue'
+
+function checkIFrame() {
+  const iframe = document.getElementById('FCL_IFRAME')
+  if (iframe && iframe.style.zIndex !== '300')
+    iframe.style.zIndex = '300'
+}
+
+onMounted(() => {
+  setInterval(checkIFrame, 1000)
+})
 </script>
 
 <template>
   <NuxtLayout>
     <NuxtPage />
+
+    <TransactionDialog
+      v-for="transaction in TransactionModals"
+      :key="transaction.transactionId"
+      :title="transaction.title"
+      :transaction-id="transaction.transactionId"
+    />
 
     <ClientOnly>
       <Notivue v-slot="item">
