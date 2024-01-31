038f6fade0304f49869bdc5cfcfa42ef8e0c80d8
Shalini <shalini5640j@gmail.com>
2024-01-31 22:05:58 +0530

    feat: update file and change rental page

---
+++ pages/rental.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+consola.info('Hello from home page', userData.value)
+
+const houses = [
+  {
+    id: nanoid(),
+    name: 'House 1',
+    image: 'https://housing.com/news/wp-content/uploads/2022/11/shutterstock_1715891752-1200x700-compressed.jpg',
+  },
+]
+</script>
+
+<template>
+  <div flex flex-col gap-8>
+    <h1 font-bold text-2xl>
+      Browse Homes
+    </h1>
+    <div grid grid-cols-3 gap-8>
+      <HouseInfo v-for="house in houses" :key="house.id" :house="house" :image="house.image" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
